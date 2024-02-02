8ca1244fafe9ee43fa828ff1a13c09ec6876b7a9
Yasin <mailtoyasi@gmail.com>
2024-02-02 08:58:19 +0530

    Initial commit for plugin development

---
+++ src/main/java/com/example/pluginhelloworld/HelloWorldAction.java
@@ -0,0 +1,28 @@
+package com.example.pluginhelloworld;
+
+import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.CaretModel;
+import com.intellij.openapi.editor.Editor;
+import org.jetbrains.annotations.NotNull;
+
+public class HelloWorldAction extends AnAction  {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        final Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        final CaretModel caretModel = editor.getCaretModel();
+        String selectedText = caretModel.getCurrentCaret().getSelectedText();
+
+        BrowserUtil.browse("https://www.google.com/search?q="+selectedText);
+    }
+
+    @Override
+    public void update(AnActionEvent e) {
+        final Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        final CaretModel caretModel = editor.getCaretModel();
+        e.getPresentation().setEnabledAndVisible(caretModel.getCurrentCaret().hasSelection());
+    }
+}
